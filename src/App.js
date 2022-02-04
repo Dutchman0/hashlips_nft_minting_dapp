9b63209153c34d43466d97b05c423a28dbeb3652
MadCunt <This@email.com>
2022-02-04 03:13:33 +0000

    removed accidental comment

--- src/App.js
+++ src/App.js
@@ -246,32 +246,8 @@ function App() {
         })
         .catch(err => {
             console.log(err);
-        });    
-
-
-    //USELESS HASLIPS CODE
-    // blockchain.smartContract.methods
-    //   .mint(mintAmount)
-    //   .send({
-    //     gasLimit: String(totalGasLimit),
-    //     to: CONFIG.CONTRACT_ADDRESS,
-    //     from: blockchain.account,
-    //     value: totalCostWei,
-    //   })
-    //   .once("error", (err) => {
-    //     console.log(err);
-    //     setFeedback("Sorry, something went wrong please try again later.");
-    //     setClaimingNft(false);
-    //   })
-    //   .then((receipt) => {
-    //     console.log(receipt);
-    //     setFeedback(
-    //       `WOW, the ${CONFIG.NFT_NAME} is yours! go visit Opensea.io to view it.`
-    //     );
-    //     setClaimingNft(false);
-    //     dispatch(fetchData(blockchain.account));
-    //   });
-  };
+        });      
+    };
 
   const decrementMintAmount = () => {
     let newMintAmount = mintAmount - 1;
